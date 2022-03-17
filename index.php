3bf151efa412cac2a832d3c81ddeac4ce91aafe6
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-03-17 14:29:19 +0100

    completed dao layer with function to supply params through url

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
 
-echo "Hello world, im testing if heroku app works"
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("rest/dao/ComicStoreDao.class.php");
+
+$dao = new ComicStoreDao();
+
+$op = $_REQUEST['op'];
+
+switch ($op) {
+  case 'insert':
+    $name = $_REQUEST['name'];
+    $description = $_REQUEST['description'];
+    $dao->add($name, $description);
+    break;
+
+  case 'delete':
+    $id = $_REQUEST['id'];
+    $dao->delete($id);
+    echo "DELETED $id";
+    break;
+
+  case 'update':
+    $id = $_REQUEST['id'];
+    $name = $_REQUEST['name'];
+    $description = $_REQUEST['description'];
+    $dao->update($id, $name, $description);
+    echo "UPDATED $id";
+    break;
+
+  case 'get':
+  default:
+    $results = $dao->get_all();
+    print_r($results);
+    break;
+}
 
 ?>
