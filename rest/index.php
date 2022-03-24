641193fef454321115ee20343aad0abdfd4e0782
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-03-24 12:23:30 +0100

    list all comics

--- rest/index.php
+++ rest/index.php
@@ -1,5 +1,24 @@
 <?php
-require '../vendor/autoload.php'; // run autoloader
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php'; // run autoloader
+require_once 'dao/ComicStoreDao.class.php';
+
+
+// Implementing CRUD operations, now through FlightPHP
+
+/**
+ * Function to list all comics
+ */
+
+Flight::route('/comics', function()
+{
+  $dao = new ComicStoreDao();
+  $comics = $dao->get_all();
+  Flight::json($comics);
+});
 
 Flight::route('/', function()  //define what fucntion will happen on / route
 {
@@ -8,4 +27,4 @@ Flight::route('/', function()  //define what fucntion will happen on / route
 
 Flight::start(); // start flight framework
 
- ?>
+?>
