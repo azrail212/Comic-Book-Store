4c5abbc4988bbd31d18ced8a64e9dc2392b937b6
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-05-20 15:46:31 +0200

    create login method

--- rest/index.php
+++ rest/index.php
@@ -6,18 +6,22 @@
   require_once __DIR__.'/../vendor/autoload.php';
   require_once __DIR__.'/services/ComicService.class.php';
   require_once __DIR__.'/services/CategoryService.class.php';
+  require_once __DIR__.'/dao/UserDao.class.php';
+
 
   //we use this method so that we don't have to call dao each time we want to use it
   Flight::register('comicService', 'ComicService');
   Flight::register('categoryService', 'CategoryService');
-//
-//   Flight::map('error', function(Exception $ex){
-//     // Handle error
-//     Flight::json(['message' => $ex->getMessage()], 500);
-// });
+  Flight::register('userDao', 'UserDao');
+
+  Flight::map('error', function(Exception $ex){
+      // Handle error
+      Flight::json(['message' => $ex->getMessage()], 500);
+  });
 
   require_once __DIR__.'/routes/ComicRoutes.php';
   require_once __DIR__.'/routes/CategoryRoutes.php';
+  require_once __DIR__.'/routes/UserRoutes.php';
 
   Flight::start(); // start flight framework
 
