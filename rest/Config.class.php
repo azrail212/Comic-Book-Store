bc3e08481aaf6a53fd2a989c010f10e7b2630696
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-05-20 10:45:02 +0200

    db update

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -3,19 +3,19 @@
 class Config {
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "localhost");
+    return Config::get_env("DB_HOST", "webprojects-do-user-11616479-0.b.db.ondigitalocean.com");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "comicbookstore");
+    return Config::get_env("DB_USERNAME", "doadmin");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "comicbookstore");
+    return Config::get_env("DB_PASSWORD", "17110000");
   }
   public static function DB_SCHEME(){
     return Config::get_env("DB_SCHEME", "comicbookstore");
   }
   public static function DB_PORT(){
-    return Config::get_env("DB_PORT", "3306");
+    return Config::get_env("DB_PORT", "25060");
   }
 
   public static function get_env($name, $default){
