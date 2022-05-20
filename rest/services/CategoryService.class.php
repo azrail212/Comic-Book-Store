7f6d65cde6f4e17d52dba8ca4bf5c4cab50943b2
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-05-20 10:33:19 +0200

    filepath fix

--- rest/services/CategoryService.class.php
+++ rest/services/CategoryService.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'../BaseService.class.php';
+require_once __DIR__.'/BaseService.class.php';
 require_once __DIR__.'/../dao/CategoryDao.class.php';
 
 class CategoryService extends BaseService{
